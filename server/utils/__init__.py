5e2f2614b983dc2430831c1a545a2be275b12ecf
professional124 <ethan.j.del5@outlook.com>
2025-06-26 10:27:55 +1000

    Update __init__.py

--- server/utils/__init__.py
+++ server/utils/__init__.py
@@ -1,1 +1,16 @@
+# server/utils/__init__.py
 
+# This file allows importing utils as a package.
+# You can import utility functions here for easier access.
+
+from .key_manager import validate_key, get_slots_for_key
+from .logger import setup_logger, log_task_event
+from .helpers import some_helper_function  # example helper import
+
+__all__ = [
+    "validate_key",
+    "get_slots_for_key",
+    "setup_logger",
+    "log_task_event",
+    "some_helper_function",
+]
